c37c1c26c6d891e6cfd68d53a392f165b973a25c
Chris Browne <112651196+ChrisBrowne01@users.noreply.github.com>
2024-10-13 17:54:31 +0100

    Displaying Chirps message posts

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -4,6 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+
 
 class Chirp extends Model
 {
@@ -12,4 +15,9 @@ class Chirp extends Model
     protected $fillable = [
         'message',
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
